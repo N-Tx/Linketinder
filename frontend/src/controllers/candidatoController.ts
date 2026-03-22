f8282b640f65f1e62711e30f36ee8da638e068a5
N-Tx <nathanoliveirafacul@gmail.com>
2026-03-22 23:03:10 +0000

    feat:adicionando validador no controller candidato

--- frontend/src/controllers/candidatoController.ts
+++ frontend/src/controllers/candidatoController.ts
@@ -1,18 +1,45 @@
 import { Candidato } from "../models/Candidato";
+import { RegexPadroes } from "../services/validador";
 
 export function cadastrarCandidato(event: Event) {
     event.preventDefault(); // Impede a página de recarregar
 
     const nome = (document.getElementById("nome") as HTMLInputElement).value;
     const email = (document.getElementById("email") as HTMLInputElement).value;
+    const cep = ((document.getElementById("cep") as HTMLInputElement).value);
+
+    // vai entrar validação do regex
+
+    if (nome.length < 3) {
+        alert("O nome deve ter pelo menos 3 caracteres.");
+        return;
+    }
+    if (nome.length < 3) {
+        alert("O nome deve ter pelo menos 3 caracteres.");
+        return;
+    }
+
+    if (!RegexPadroes.email.test(email)) {
+        alert("E-mail inválido! Ex: usuario@email.com");
+        return;
+    }
+
+    if (!RegexPadroes.cep.test(cep)) {
+        alert("CEP inválido! Use o formato 00000-000.");
+        return;
+    }
+
+
+
+
+
     const idade = Number((document.getElementById("idade") as HTMLInputElement).value);
     const estado = (document.getElementById("estado") as HTMLInputElement).value;
-    const cep = Number((document.getElementById("cep") as HTMLInputElement).value);
     const descricao = (document.getElementById("descricao") as HTMLInputElement).value;
     const skills = (document.getElementById("skills") as HTMLInputElement).value.split(",");
 
     // O .map(s => s.trim()) serve para tirar os espaços das skills (ex: "Java, TS" vira "Java" e "TS")
-    const novo = new Candidato(nome, email, idade, estado, cep, descricao, skills.map(s => s.trim()));
+    const novo = new Candidato(nome, email, idade, estado, Number(cep), descricao, skills.map(s => s.trim()));
 
     // A MÁGICA: Busca os candidatos salvos ou cria lista vazia
     const candidatosSalvos = JSON.parse(localStorage.getItem("candidatos") || "[]");
--- frontend/src/services/validador.ts
+++ frontend/src/services/validador.ts
@@ -4,6 +4,5 @@ export const RegexPadroes = {
     cpf: /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/,
     cnpj: /^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$/,
     telefone: /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/,
-    cep: /^\d{5}-?\d{3}$/,
-    linkedin: /^https:\/\/(www\.)?linkedin\.com\/in\/[a-z0-9_-]+\/?$/i
+    cep: /^\d{5}-?\d{3}$/
 };
