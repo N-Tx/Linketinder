cfdd8c4b39fb5b732f1b949e39d091398c7275d0
N-Tx <nathanoliveirafacul@gmail.com>
2026-03-25 01:01:51 +0000

    fix: mudando alguns comentarios e att algumas coisas

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,14 +4,14 @@ import { cadastrarVaga } from "./controllers/vagaController";
 import { listarVagas, listarCandidatosAnonimo, deletarVaga } from "./controllers/listagem";
 import { gerarGrafico } from "./charts/chart";
 
-// Conectando com o HTML
+
 (window as any).cadastrarCandidato = cadastrarCandidato;
 (window as any).cadastrarEmpresa = cadastrarEmpresa;
 (window as any).cadastrarVaga = cadastrarVaga;
 (window as any).deletarVaga = deletarVaga;
 (window as any).gerarGrafico = gerarGrafico;
 
-// Isso faz com que as listas sejam carregadas assim que você entra na página de Perfil
+// isso faz com que as listas sejam carregadas assim que eu entro na página de Perfil
 window.onload = () => {
     listarVagas();
     listarCandidatosAnonimo();
